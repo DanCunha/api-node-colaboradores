8c2a374fc97bb5cf14a99c33328a7c27ff5b0e5c
Daniel <daniel.cunha@fabriq.com.br>
2018-05-17 12:25:27 -0400

    Configuracao da conexao com o BD do Heroku

--- persistencia/connectionFactory.js
+++ persistencia/connectionFactory.js
@@ -1,12 +1,25 @@
 var mysql  = require('mysql');
 
 function createDBConnection(){
+	if (!process.env.NODE_ENV || process.env.node === 'dev') {
 		return mysql.createConnection({
 			host: 'localhost',
 			user: 'root',
 			password: '',
 			database: 'empresa'
 		});
+	}
+		
+	if (process.env.NODE_ENV == 'production') {
+		var url = process.env.CLEARDB_DATABASE_URL;
+		var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+		return mysql.createConnection({
+			host:grupos[3],
+			user:grupos[1],
+			password:grupos[2],
+			database:grupos[4]
+		});
+	}
 }
 
 module.exports = function() {
